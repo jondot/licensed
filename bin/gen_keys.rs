97785b83ad8110e8bcb071735645fca023e9d7e8
Dotan Nahum <jondotan@gmail.com>
2019-05-28 22:06:38 +0300

    update crypto deps to latest

--- bin/gen_keys.rs
+++ bin/gen_keys.rs
@@ -1,6 +1,7 @@
 extern crate ring;
 extern crate untrusted;
 
+use ring::signature::KeyPair;
 use std::env;
 use std::fs;
 use std::process::exit;
@@ -20,16 +21,22 @@ fn main() -> Result<(), String> {
     };
     // Generate a key pair in PKCS#8 (v2) format.
     let rng = rand::SystemRandom::new();
-    let pkcs8_bytes = signature::Ed25519KeyPair::generate_pkcs8(&rng).map_err(|e| format!("Error: {:?}", e))?;
+    let pkcs8_bytes =
+        signature::Ed25519KeyPair::generate_pkcs8(&rng).map_err(|e| format!("Error: {:?}", e))?;
 
     // Normally the application would store the PKCS#8 file persistently. Later
     // it would read the PKCS#8 file from persistent storage to use it.
 
     let key_pair =
-    signature::Ed25519KeyPair::from_pkcs8(
-                untrusted::Input::from(&pkcs8_bytes)).map_err(|e| format!("Error: {:?}", e))?;
-    fs::write(&format!("{}/private.pks", key_path), pkcs8_bytes.as_ref()).map_err(|e| format!("FS Error: {:?}", e))?;
+        signature::Ed25519KeyPair::from_pkcs8(untrusted::Input::from(pkcs8_bytes.as_ref()))
+            .map_err(|e| format!("Error: {:?}", e))?;
+    fs::write(&format!("{}/private.pks", key_path), pkcs8_bytes.as_ref())
+        .map_err(|e| format!("FS Error: {:?}", e))?;
 
-    fs::write(&format!("{}/public.pks", key_path), &key_pair.public_key_bytes()).map_err(|e| format!("FS Error: {:?}", e))?;
+    fs::write(
+        &format!("{}/public.pks", key_path),
+        &key_pair.public_key().as_ref(),
+    )
+    .map_err(|e| format!("FS Error: {:?}", e))?;
     Ok(())
 }
